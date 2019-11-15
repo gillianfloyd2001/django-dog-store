211472d3c35891c459ee2a79b6cc085ace72c7c9
Student <student@donation.local>
2019-11-15 15:50:17 -0600

    getting the redirect to work

--- app/views.py
+++ app/views.py
@@ -12,20 +12,23 @@ def home(request):
 
 
 def dog_product_detail(request, dog_product_id):
-    dog_product = DogProduct.objects.get(id=dog_product_id)
-    return render(request, "dog_product_detail.html", {"dog_prduct": dog_product})
+    product = DogProduct.objects.get(id=dog_product_id)
+    return render(request, "dog_product_detail.html", {"dog_product": product})
 
 
 def purchase_dog_product(request, dog_product_id):
     dog_product = DogProduct.objects.get(id=dog_product_id)
-    if dog_product_id.in_stock:
-        DogProduct.quantity - 1
-        purchase = dog_product.purchase_set.create(datetime=datetime)
-        messages.success(request, f"Purchase {DogProduct.name}")
-        return redirect("purchase_detail")
+    if dog_product.quantity != 0:
+        dog_product.quantity -= 1
+        dog_product.save()
+        purchase = Purchase.objects.create(
+            dog_product=dog_product, purchased_at=datetime.now()
+        )
+        messages.success(request, f"Purchased {dog_product.name}")
+        return redirect("purchase_detail", purchase.id)
     else:
-        messages.error(request, f"{DogProduct.name} is out of stock")
-        return redirect("purchase_detail")
+        messages.error(request, f"{dog_product.name} is out of stock")
+        return redirect("dog_product_detail", dog_product_id)
 
 
 def purchase_detail(request, purchase_id):
